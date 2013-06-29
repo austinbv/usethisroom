1dddcf90c19afae28dfe855a71a171152f6f497e
Austin <austinbv@gmail.com>
2013-06-29 00:15:31 -0600

    initial commit

---
+++ src/main/java/com/austinbv/usethisroom/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package com.austinbv.usethisroom;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.austinbv.usethisroom")
+public class ApplicationConfiguration {
+
+}
